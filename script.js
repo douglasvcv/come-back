265ea0551253dc5869abc4128fcb5c66929fe7f7
Douglas <douglasvitor1233@gmail.com>
2024-09-03 23:28:03 -0300

    come back:)

---
+++ script.js
@@ -0,0 +1,10 @@
+const num = [[0,0,0], [0,1,1]]
+
+for(let i = 0; i<num.length; i++){
+    for(let j = 0; j < num[i].length; j++){
+        let numInside = num[i]
+        if(numInside[j] = 0){
+            
+        }
+    }
+}
